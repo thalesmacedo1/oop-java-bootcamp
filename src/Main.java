a7781936fbcdb8dbece1f93fc450b1d500e98790
Thales Macêdo <thalesmacedo1@live.com>
2022-01-19 20:39:47 -0300

    Implementation in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import com.bootcampabstraction.challenge.domain.Bootcamp;
 import com.bootcampabstraction.challenge.domain.Course;
+import com.bootcampabstraction.challenge.domain.Dev;
 import com.bootcampabstraction.challenge.domain.Mentorship;
 
 import java.time.LocalDate;
@@ -20,8 +22,29 @@ public class Main {
         mentorship1.setDescription("Create a perfect roadmap for your career with the advice of experts.");
         mentorship1.setDate(LocalDate.now());
 
-        System.out.println(course1);
-        System.out.println(course2);
-        System.out.println(mentorship1);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setName("Java Developer Bootcamp");
+        bootcamp.setDescription("Develop backend applications faster than expected");
+        bootcamp.getContent().add(course1);
+        bootcamp.getContent().add(course2);
+        bootcamp.getContent().add(mentorship1);
+
+        Dev devThales = new Dev();
+        devThales.setName("Thales");
+        devThales.subscribeBootcamp(bootcamp);
+        System.out.println("Thales' Assigned Contents: " + devThales.getSubscribedContents());
+        devThales.advance();
+        devThales.advance();
+        System.out.println("Thales' Completed Contents: " + devThales.getFinishedContents());
+        System.out.println("XP: " + devThales.calculateXp());
+
+        Dev devMatt = new Dev();
+        devMatt.setName("Matt");
+        devMatt.subscribeBootcamp(bootcamp);
+        System.out.println("Matt's Assigned Contents: " + devMatt.getSubscribedContents());
+        devMatt.advance();
+        System.out.println("Matt's Completed Contents: " + devMatt.getFinishedContents());
+        System.out.println("XP: " + devMatt.calculateXp());
+
     }
 }
